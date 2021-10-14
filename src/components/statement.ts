a3d716fb52c6b05d9caca8878c31a715dfc860fc
Sergey-Kupchik <kupchikrabota@gmail.com>
2021-10-14 15:04:21 -0400

    added renderPlainText()

--- src/components/statement.ts
+++ src/components/statement.ts
@@ -3,78 +3,79 @@ import { invoices, plays } from './state';
 import { InvoiceType, PerformanceType, PlayObjType, PlaysObjType } from './types';
 
 
-function amountFor(aPerformance: PerformanceType) {
-    let result = 0;
-    switch (playFor(aPerformance).type) {
-        case 'tragedy':
-            result = 40000;
-            if (aPerformance.audience > 30) {
-                result += 1000 * (aPerformance.audience - 30)
-            }
-            break
-        case 'comedy':
-            result = 30000;
-            if (aPerformance.audience > 20) {
-                result += 10000 + 500 * (aPerformance.audience - 20)
-            }
-            result += 300 * aPerformance.audience;
-            break
-        default:
-            throw new Error(`unknown type: ${playFor(aPerformance).type}`);
-    }
-    return result;
-}
 
-function playFor(aPerformance: PerformanceType): PlayObjType {
-    return plays[aPerformance.playID]
-}
 
-function volumeCreditsFor(aPerformance: PerformanceType) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ('comedy' === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
-    return result
-}
-
-function usd(aNumber: number) {
-    return new Intl.NumberFormat('en-US',
-        {
-            style: 'currency',
-            currency: 'USD',
-            minimumFractionDigits: 2
-        }).format(aNumber/ 100)
-}
-
-export function statement(invoice: InvoiceType, plays: PlaysObjType) {
-
+function renderPlainText(invoice: InvoiceType, plays: PlaysObjType) {
     let result = `Statement for ${invoice.customer}\n`;
-
     for (let perf of invoice.performances) {
-        //print line for this order 
         result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     }
-
     result += `Amount owed is ${usd(totalAmount())}\n`;
     result += `You earned $${totalVolumeCredits()} credits\n`;
-
     return result
 
-    function totalVolumeCredits (){
+    function totalVolumeCredits() {
         let result = 0;
         for (let perf of invoice.performances) {
             result += volumeCreditsFor(perf);
         }
-        return result; 
+        return result;
     }
 
-    function totalAmount () {
+    function totalAmount() {
         let result = 0;
         for (let perf of invoice.performances) {
             result += amountFor(perf);
         }
-        return result 
+        return result
+    }
+
+    function amountFor(aPerformance: PerformanceType) {
+        let result = 0;
+        switch (playFor(aPerformance).type) {
+            case 'tragedy':
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30)
+                }
+                break
+            case 'comedy':
+                result = 30000;
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20)
+                }
+                result += 300 * aPerformance.audience;
+                break
+            default:
+                throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+        }
+        return result;
+    }
+
+    function playFor(aPerformance: PerformanceType): PlayObjType {
+        return plays[aPerformance.playID]
+    }
+
+    function volumeCreditsFor(aPerformance: PerformanceType) {
+        let result = 0;
+        result += Math.max(aPerformance.audience - 30, 0);
+        if ('comedy' === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+        return result
+    }
+
+    function usd(aNumber: number) {
+        return new Intl.NumberFormat('en-US',
+            {
+                style: 'currency',
+                currency: 'USD',
+                minimumFractionDigits: 2
+            }).format(aNumber / 100)
     }
 }
+
+export function  statement (invoice: InvoiceType, plays: PlaysObjType) {
+    return renderPlainText(invoice, plays)
+}; 
 
 let brutto = statement(invoices, plays);
 

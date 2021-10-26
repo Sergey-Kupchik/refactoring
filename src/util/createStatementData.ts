de8386b6b0f97d012b27c015123cf256570f9571
Sergey-Kupchik <kupchikrabota@gmail.com>
2021-10-26 13:27:45 -0400

    moved amount to class

--- src/util/createStatementData.ts
+++ src/util/createStatementData.ts
@@ -13,7 +13,7 @@ function createStatementData(invoice: InvoiceType, plays: PlaysObjType) {
         const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance))
         const result = Object.assign({}, aPerformance)
         result.play = calculator.play;
-        result.amount = amountFor(result);
+        result.amount = calculator.amount;
         result.volumeCredits = volumeCreditsFor(result);
         return result
     }
@@ -21,27 +21,7 @@ function createStatementData(invoice: InvoiceType, plays: PlaysObjType) {
     function playFor(aPerformance: PerformanceType): PlayObjType {
         return plays[aPerformance.playID]
     }
-    function amountFor(aPerformance: aPerformanceType) {
-        let result = 0;
-        switch (aPerformance.play.type) {
-            case 'tragedy':
-                result = 40000;
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30)
-                }
-                break
-            case 'comedy':
-                result = 30000;
-                if (aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20)
-                }
-                result += 300 * aPerformance.audience;
-                break
-            default:
-                throw new Error(`unknown type: ${aPerformance.play.type}`);
-        }
-        return result;
-    }
+
     function volumeCreditsFor(aPerformance: aPerformanceType) {
         let result = 0;
         result += Math.max(aPerformance.audience - 30, 0);
@@ -58,10 +38,31 @@ function createStatementData(invoice: InvoiceType, plays: PlaysObjType) {
 
 class PerformanceCalculator {
     performance: aPerformanceType;
-    play: PlayObjType
+    play: PlayObjType;
     constructor(aPerformance: aPerformanceType, aPlay: PlayObjType) {
         this.performance = aPerformance
         this.play = aPlay
+    }
+    get amount() {
+        let result = 0;
+        switch (this.play.type) {
+            case 'tragedy':
+                result = 40000;
+                if (this.performance.audience > 30) {
+                    result += 1000 * (this.performance.audience - 30)
+                }
+                break
+            case 'comedy':
+                result = 30000;
+                if (this.performance.audience > 20) {
+                    result += 10000 + 500 * (this.performance.audience - 20)
+                }
+                result += 300 * this.performance.audience;
+                break
+            default:
+                throw new Error(`unknown type: ${this.play.type}`);
+        }
+        return result;
     }
 }
 export default createStatementData;
